27dd31adb0a396e0555db25842c8f44d4d909aa4
aby <jiangyangjing@hetao101.com>
2020-07-07 01:27:49 +0800

    创建结构

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import './index.less';
+
+class Background extends Component {
+
+  render() {
+    return (
+      <div className="structure_contain">
+        <h2 className="structure_title">组件目录</h2>
+        <div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Background;
